ab685d227ae08467a880100000e7de100d7ad9aa
Cameron Myron <cmm8535@g.rit.edu>
2023-01-09 09:03:52 -0500

    Added a CLI with a couple commands. Still need to add the "help" command and add cryptography tools (you know, because that is the point of this project).

--- Crypto_CLI/Crypto_CLI/src/Main.cpp
+++ Crypto_CLI/Crypto_CLI/src/Main.cpp
@@ -1,9 +1,59 @@
+#include <map>
 #include <iostream>
+#include <iomanip>
 #include "buildinfo.h"
 
-int main(int, char* [])
+#define MAX_CMD_SIZE 200
+
+static enum commands
 {
-      printf("Just a test to make sure I set up the project correctly (for the most part).\n");
-      printf("%s\n",timestamp);
-      return 0;
+   INVALID,
+   INFO,
+   EXIT
+};
+
+static std::map<std::string, commands> strToCommandMap;
+
+static void getBuildInfo()
+{
+   std::cout << "Build Timestamp: " << timestamp << std::endl << std::endl;
+}
+
+static void invalidCmd()
+{
+   std::cout << "Invalid command. Type \"help\" to see valid options" << std::endl << std::endl;
+}
+
+int main(int, char**)
+{
+   // Init
+
+   // Make char* to command map
+   strToCommandMap["info"] = commands::INFO;
+   strToCommandMap["exit"] = commands::EXIT;
+
+   std::string cmd;
+
+   // Runtime Loop
+   while (1)
+   {
+      // Get Command Input
+      std::cout << "Main > ";
+      std::cin >> cmd;
+      std::cout << std::endl;
+
+      // Running The Given Command
+      switch (strToCommandMap[cmd])
+      {
+         case commands::INFO:
+            getBuildInfo();
+            break;
+         case commands::EXIT:
+            return 0;
+            break;
+         default:
+            invalidCmd();
+            break;
+      }
+   }
 }
